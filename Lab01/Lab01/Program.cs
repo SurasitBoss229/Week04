b5ab996b205a7a4235160a0cd788f331a7313363
Surasit <surasitboss229@gmail.com>
2017-09-07 12:59:45 +0700

    W4P04

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,9 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("This is text 1.");
+            Console.WriteLine("This is text 2.");
+            Console.WriteLine("This is text 3.");
         }
     }
 }
