0ef6ddf32636360c2434f35fa0b8a7b2a62443fb
Surasit <surasitboss229@gmail.com>
2017-09-07 13:39:37 +0700

    W4P09

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,8 +7,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);
         }
     }
 }
